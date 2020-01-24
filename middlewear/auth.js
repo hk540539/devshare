26556c8d0781761527c334f44caf77a5a7c1e7fd
HK <hk540539@gmail.com>
2020-01-24 21:12:57 +0530

    auth route changes

--- middlewear/auth.js
+++ middlewear/auth.js
@@ -1,27 +1,27 @@
-const config = require("config");
-const jwt = require("jsonwebtoken");
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  //Get token from header
-  const token = req.header("x-auth-token");
+	//Get token from header
+	const token = req.header('x-auth-token');
 
-  // check if token exists
-  if (!token) {
-    return res.status(401).json({ msg: "no token authorization denied" });
-  }
+	// check if token exists
+	if (!token) {
+		return res.status(401).json({ msg: 'no token authorization denied' });
+	}
 
-  //verify token
+	//verify token
 
-  try {
-    // decoding the token which we received from header
-    const decoded = jwt.verify(token, config.get("JWTSecretKey"));
-    // Here we r setting req.user value to decoded.user,
-    // Where decoded.user value is same as the payload.
-    // User which contains user id details
-    req.user = decoded.user;
-    next();
-  } catch (error) {
-    // token is available and not valid
-    res.status(401).json({ msg: "Token is not valid" });
-  }
+	try {
+		// decoding the access token which we pass it to haeder along with secret key
+		const decoded = jwt.verify(token, config.get('JWTSecretKey'));
+		// Here we r setting req.user value to decoded.user,
+		// Where decoded.user value is same as the payload.
+		// User which contains user id details
+		req.user = decoded.user;
+		next();
+	} catch (error) {
+		// token is available and not valid
+		res.status(401).json({ msg: 'Token is not valid' });
+	}
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,21 +1,76 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const auth = require("../../middlewear/auth");
-const User = require("../../models/Users");
+const auth = require('../../middlewear/auth');
+const User = require('../../models/Users');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //@route       GET api/auth
 //@description test route
 //@access      Private
-router.get("/", auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("server error");
-  }
+
+// header should be passed for auth middlewear to get req.user.id
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send('server error');
+	}
 });
+
+//@route       POST api/auth
+//@description Authniticate User and get token
+//@access      Public
+router.post(
+	'/',
+	[
+		// username must be an email
+		check('email', 'please include valid email').isEmail(),
+		// password must be at least 5 chars long
+		check('password', 'Password is required').exists()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		console.log(req.body);
+		const { email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({ erros: [ { msg: 'Invalid Credentials' } ] });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(400).json({ erros: [ { msg: 'Invalid Credentials' } ] });
+			}
+
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(payload, config.get('JWTSecretKey'), { expiresIn: 36000 }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+
+			//	res.send('User registered');
+		} catch (error) {
+			console.log(error.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
